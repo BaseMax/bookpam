c9436ec312e000ae8c1d358f646b78aab0cafb84
Max Base <MaxBaseCode@Gmail.Com>
2020-12-15 20:58:29 +0330

    Adding functions list, cover, etc...

--- bookpam.py
+++ bookpam.py
@@ -1,18 +1,38 @@
 # Max Base
-# 2020-12-13
+# 2020-12-13, 2020-12-14, 2020-12-15
 # https://github.com/BaseMax/bookpam
 
 import re
 
-filename = 'book.md'
 filename = 'test.md'
 inputFile = open(filename, 'r')
 data = inputFile.read().strip()
 
+cover = ""
+fehrest = ""
 output = ""
+functions_papers = ""
+functions = []
+
+def appending(s):
+	global cover
+	global output
+	if start_book_pages == 0:
+		cover += s
+	else:
+		output += s
+
+def rep_func(gp):
+	if gp[1] not in functions:
+		functions.append(gp[1])		
+	# print(gp[1])
+	return '<kbd class="function">' + gp[1] + '</kbd>'
+	
 
 lines = data.split("\n")
 
+start_book_pages = 0 # detect it's fehrest or not!
+headers = []
 status = 0
 countLines = len(lines)
 i = 0
@@ -20,36 +40,58 @@ while i < countLines:
 	lines[i] = lines[i].strip()
 	if lines[i].startswith('```'):
 		if status == 0:
-			output += '<pre><code>\n';
+			appending('<pre><code>\n')
 			status = 1
 		elif status == 1:
-			output += '</code></pre>';
+			appending('</code></pre>')
 			status = 0
 		i = i+1
 		continue
 	if status == 1:
-		output += lines[i]+"\n"
+		appending(lines[i]+"\n")
 		i = i+1
 		continue
 	lines[i] = re.sub(r'\{([^\}])\}', r'<img src="\1">', lines[i])
 	if lines[i].startswith('######'):
-		lines[i] = '<h6>' + lines[i][6:].strip() + '</h6>'
+		value = lines[i][6:].strip()
+		lines[i] = '<h6>' + value + '</h6>'
+		if start_book_pages == 1:
+			headers.append((6, value))
 	if lines[i].startswith('#####'):
-		lines[i] = '<h5>' + lines[i][5:].strip() + '</h5>'
+		value = lines[i][5:].strip()
+		lines[i] = '<h5>' + value + '</h5>'
+		if start_book_pages == 1:
+			headers.append((5, value))
 	if lines[i].startswith('####'):
-		lines[i] = '<h4>' + lines[i][4:].strip() + '</h4>'
+		value = lines[i][4:].strip()
+		lines[i] = '<h4>' + value + '</h4>'
+		if start_book_pages == 1:
+			headers.append((4, value))
 	if lines[i].startswith('###'):
-		lines[i] = '<h3>' + lines[i][3:].strip() + '</h3>'
+		value = lines[i][3:].strip()
+		lines[i] = '<h3>' + value + '</h3>'
+		if start_book_pages == 1:
+			headers.append((3, value))
 	if lines[i].startswith('##'):
-		lines[i] = '<h2>' + lines[i][2:].strip() + '</h2>'
+		value = lines[i][2:].strip()
+		lines[i] = '<h2>' + value + '</h2>'
+		if start_book_pages == 1:
+			headers.append((2, value))
 	if lines[i].startswith('#'):
-		lines[i] = '<h1>' + lines[i][1:].strip() + '</h1>'
+		value = lines[i][1:].strip()
+		lines[i] = '<h1>' + value + '</h1>'
+		if start_book_pages == 1:
+			headers.append((1, value))
 	lines[i] = lines[i].replace('[center]', '<center>')
 	lines[i] = lines[i].replace('[/center]', '</center>')
+	if start_book_pages == 0 and '[page]' in lines[i]:
+		start_book_pages = 1
 	lines[i] = lines[i].replace('[page]', '<section>')
 	lines[i] = lines[i].replace('[/page]', '</section>')
+	lines[i] = lines[i].replace('[cover]', '<section>')
+	lines[i] = lines[i].replace('[/cover]', '</section>')
 	lines[i] = re.sub(r'\{([^\}]+)\}', r'<img src="\1">', lines[i])
-	lines[i] = re.sub(r'\<\<([^\>]+)\>\>', r'<kbd class="function">\1</kbd>', lines[i])
+	lines[i] = re.sub(r'\<\<([^\>]+)\>\>', rep_func, lines[i])
 	lines[i] = re.sub(r'\`([^\`]+)\`', r'<kbd class="variable">\1</kbd>', lines[i])
 	# print("==>'"+ lines[i] +"'")
 	if lines[i] == "":
@@ -58,15 +100,38 @@ while i < countLines:
 				i+=1
 		except:
 			pass
-		output += "<br>\n"
+		appending("<br>\n")
 	else:
-		output += lines[i]
+		appending(lines[i])
 	i += 1
 # print(data)
 
 layoutFile = open('layout.html', 'r')
 layoutData = layoutFile.read()
+
+styleFile = open('style.css', 'r')
+styleData = styleFile.read()
+
+scriptFile = open('script.js', 'r')
+scriptData = scriptFile.read()
+
+# for
+# print(headers)
+fehrest = '<section class="fehrest"><h1>فهرست</h1><ul>'
+for header in headers:
+	fehrest += '<li class="sub-' + str(header[0]) + '">' + header[1] + '</li>'
+fehrest += "</ul></section>"
+
+functions_papers = '<section class="functions"><h1>توابع</h1><ul>'
+for function in functions:
+	functions_papers += '<li>' + function + '</li>'
+functions_papers += "</ul></section>"
+
+output = cover + fehrest + output + functions_papers
+
 layoutData = layoutData.replace('{{title}}', 'گپ: از نظریه گروه تا برنامه نویسی')
+layoutData = layoutData.replace('{{style}}', styleData)
+layoutData = layoutData.replace('{{script}}', scriptData)
 layoutData = layoutData.replace('{{content}}', output)
 
 outputFile = open('output.html', 'w')
